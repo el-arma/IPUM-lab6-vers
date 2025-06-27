73cac4962cf71e3085fddf1be98e9e78e4021c9e
el-arma <>
2025-06-27 18:03:45 +0200

    ex.1 Beg. add .dvc/config file

--- ames_inspect_data.py
+++ ames_inspect_data.py
@@ -2,13 +2,14 @@ import click
 import pandas as pd
 
 
-@click.command()
-@click.option(
-    "--file-path",
-    required=True,
-    type=click.Path(exists=True),
-    help="Path to the file with Ames housing data.",
-)
+# @click.command()
+# @click.option(
+#     "--file-path",
+#     required=True,
+#     type=click.Path(exists=True),
+#     help="Path to the file with Ames housing data.",
+# )
+
 def inspect_ames_data(file_path: str) -> None:
     df = pd.read_parquet(file_path)
 
@@ -16,4 +17,5 @@ def inspect_ames_data(file_path: str) -> None:
 
 
 if __name__ == "__main__":
-    inspect_ames_data()
+    my_path = r'C:\Users\eligi\Desktop\inne\my_scripts\PODYPLOMOWE\sem_II\IPUM\LAB\lab_06_versioning\data\ames_data_2006_2008.parquet'
+    inspect_ames_data(my_path)
